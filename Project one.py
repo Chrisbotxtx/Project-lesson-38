d984de40a2a9b0dabb6e7783ab5b8ed0d49bf587
Chrisbotxtx <christopher.mwakasegecmj@gmail.com>
2024-10-30 21:02:37 +0300

    Project for lesson 38

---
+++ Project one.py
@@ -0,0 +1,14 @@
+a=5
+b=10
+c=15
+
+print("Before swapping a is",a,", b is",b," and c is",c)
+
+#swapping
+num=a
+a=c
+temp=b
+c=temp
+b=num
+
+print("After swapping a is",a,", b is",b," and c is",c)
